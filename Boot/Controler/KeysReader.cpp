19ef5a42c3efc0f11f96b2c708b1224a0c70ae45
christoph65 <christoph.kunz65@gmail.com>
2020-12-21 16:14:02 +0100

    Added KeysReader Class

---
+++ Boot/Controler/KeysReader.cpp
@@ -0,0 +1,72 @@
+/*
+* KeysReader.cpp
+*
+* Created: 21.05.2020 10:22:53
+* Author: Christoph
+*/
+
+
+#include "KeysReader.h"
+#include <avr/io.h>
+#include <stdio.h>
+
+// default constructor
+void KeysReader::Initialize()
+{
+	// Init all for the Switsches on PD6 and PD7 as Input
+	// Normally PULLUp is already enabled for all ports
+	DDRD &= ~((1<<PIND7) | (1<<PIND6));
+	PORTD |= (1<<PIND7) | (1<<PIND6);
+	MCUCR &= ~(1 << PUD);
+	bSwitchUpPressed = false;
+	bSwitchDownPressed = false;
+	iSwitchUpDelay = 0;
+	iSwitchDownDelay = 0;
+
+} //KeysReader
+
+void KeysReader::ReadKeys()
+{
+	// Read the Keys
+	
+	// Key must remain in one position for at least 10 Cycles
+	if ((PIND & (1 << PIND7)) > 0) { // PD7 is pressed
+		if (bSwitchDownPressed == false) iSwitchDownDelay = 0;
+		else iSwitchDownDelay += 1;
+		
+		if(iSwitchDownDelay >= 10) {
+			bSwitchDownPressed = false;
+			bSwitchDownChange = true;
+			iSwitchDownDelay = 0;
+		}
+		} else {
+		if (bSwitchDownPressed == true) iSwitchDownDelay = 0;
+		else iSwitchDownDelay += 1;
+		
+		if(iSwitchDownDelay >= 10) {
+			bSwitchDownPressed = true;
+			bSwitchDownChange = true;
+			iSwitchDownDelay = 0;
+		}
+	}
+
+	if ((PIND & (1 << PIND6)) > 0) { // PD6 is pressed
+		if (bSwitchUpPressed == false) iSwitchUpDelay = 0;
+		else iSwitchUpDelay += 1;
+		
+		if(iSwitchUpDelay >= 10) {
+			bSwitchUpPressed = false;
+			bSwitchUpChange = true;
+			iSwitchUpDelay = 0;
+		}
+		} else {
+		if (bSwitchUpPressed == true) iSwitchUpDelay = 0;
+		else iSwitchUpDelay += 1;
+		
+		if(iSwitchUpDelay >= 10) {
+			bSwitchUpPressed = true;
+			bSwitchUpChange = true;
+			iSwitchUpDelay = 0;
+		}
+	}
+}
